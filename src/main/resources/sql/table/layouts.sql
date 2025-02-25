6e71fb71204907ecccdd1af4ccaf024dcd28b7cd
david <mrminecraft00@gmail.com>
2025-02-25 20:30:46 +0100

    Refactor layouts table schema to enforce unique constraints
    Added a unique constraint on the combination of 'id' and 'chart' to ensure data integrity. Adjusted formatting for better readability and alignment. No changes made to data types or existing functionality.

--- src/main/resources/sql/table/layouts.sql
+++ src/main/resources/sql/table/layouts.sql
@@ -1,11 +1,12 @@
 CREATE TABLE IF NOT EXISTS layouts
 (
-    id    INTEGER PRIMARY KEY NOT NULL,
-    chart TEXT                NOT NULL,
-    name  TEXT                NOT NULL,
-    type  TEXT                NOT NULL,
-    color TEXT                NOT NULL,
+    id    INTEGER NOT NULL,
+    chart TEXT    NOT NULL,
+    name  TEXT    NOT NULL,
+    type  TEXT    NOT NULL,
+    color TEXT    NOT NULL,
     icon  TEXT,
     size  INTEGER,
+    UNIQUE (id, chart),
     FOREIGN KEY (id) REFERENCES projects (id) ON DELETE CASCADE
 )
