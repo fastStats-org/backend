e23d4655b7b129ff0ce09f87e5652ff5f720eccb
david <mrminecraft00@gmail.com>
2025-03-06 15:56:59 +0100

    Fix incorrect column name in set_chart_id.sql
    Changed the column name from 'id' to 'chart' in the UPDATE statement to address a mistake in the original query. This ensures the query updates the correct column as intended.

--- src/main/resources/sql/update/set_chart_id.sql
+++ src/main/resources/sql/update/set_chart_id.sql
@@ -1,5 +1,5 @@
 UPDATE layouts
-SET id = ?
+SET chart = ?
 WHERE chart = ?
   AND id = ?
   AND EXISTS (SELECT 1 FROM projects WHERE projects.owner = coalesce(?, projects.owner) AND projects.id = layouts.id);
