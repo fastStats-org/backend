e6f0ffcbd3304cbecfd9a971e80ff780bdb7a231
david <mrminecraft00@gmail.com>
2025-03-07 19:18:47 +0100

    Refactor SQL to include layout data in project queries.
    This change modifies the `get_projects` SQL query to join project data with layout information. It ensures that layout details, including charts, are retrieved alongside projects, enhancing the query's utility for related data fetching.

--- src/main/resources/sql/query/get_projects.sql
+++ src/main/resources/sql/query/get_projects.sql
@@ -1,4 +1,9 @@
-SELECT * FROM projects
-WHERE owner = coalesce(?, owner)
-  AND private = coalesce(?, private)
-LIMIT ? OFFSET ?
+SELECT project.*, layout.*
+FROM (SELECT *
+      FROM projects
+      WHERE owner = coalesce(?, owner)
+        AND private = coalesce(?, private)
+      LIMIT ? OFFSET ?) project
+         LEFT JOIN layouts layout
+                   ON layout.id = project.id
+                       AND layout.chart = project.preview_chart
